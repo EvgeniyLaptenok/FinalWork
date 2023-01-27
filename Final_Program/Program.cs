b50ec972533e84dc9229cd9e84cec256ace502fd
evgeniy <shtorm20300@gmail.com>
2023-01-27 21:54:36 +0600

    создание массива

---
+++ Final_Program/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество элементов: ");
+int size = int.Parse(Console.ReadLine()!);
+string[] array = CreatArray(size);
+
+string[] CreatArray(int size)
+{
+    string[] array = new string[size];
+    string[] arrayNew = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-й элемент: ");
+        array[i] = Console.ReadLine()!;
+        if (array[i].Length <= 3)
+        {
+            arrayNew[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref arrayNew, count);
+    return arrayNew;
+}
+
+
+
